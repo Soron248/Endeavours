05e7e8763fcc40eb51e9ee43b95a457ac33533ba
Iftekhar Jahan Soron <67633243+Soron248@users.noreply.github.com>
2024-09-22 12:09:09 +0600

    blog pagination changes

--- src/pages/BlogPages/BlogPage.jsx
+++ src/pages/BlogPages/BlogPage.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Layout } from "../../layouts/Layout";
 import { BlogPageWrapper } from "../../components/BlogAreas/BlogPageWrapper";
-import { BLOG_PAGE_LIST } from "../../mocks/blogs";
 import { Link } from "react-router-dom";
 
 const BlogPage = () => {
   const [blogs, setBlogs] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const blogsPerPage = 5; // Change this value to set how many blogs per page
 
   useEffect(() => {
-    // Fetch data when component mounts
     const fetchBlogs = async () => {
       try {
         const response = await fetch('https://endeavours.pythonanywhere.com/api/blogs/');
@@ -16,73 +16,73 @@ const BlogPage = () => {
           throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        // console.log(data)
-        setBlogs(data);  // Store the response array in state
+        setBlogs(data);
       } catch (error) {
         console.error('Error fetching blogs:', error);
       }
     };
 
     fetchBlogs();
-  }, []); 
+  }, []);
+
+  // Calculate total pages
+  const totalPages = Math.ceil(blogs.length / blogsPerPage);
+
+  // Get current blogs for the page
+  const indexOfLastBlog = currentPage * blogsPerPage;
+  const indexOfFirstBlog = indexOfLastBlog - blogsPerPage;
+  const currentBlogs = blogs.slice(indexOfFirstBlog, indexOfLastBlog);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <Layout breadcrumb={"Blog"} title={"Latest Blog"}>
       <BlogPageWrapper>
         <div className="blog-post-wrap">
-          {/* list */}
           <div className="row">
-            {blogs.map((blog) => {
+            {currentBlogs.map((blog) => {
               let dateString = blog.created_at;
-
-              // Convert to a JavaScript Date object
               let date = new Date(dateString);
-              
-              // Define an array of month names
               const months = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May.', 'Jun.', 'Jul.', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'];
-              
-              // Get the day and month from the date object
               const day = date.getDate();
-              const month = months[date.getMonth()]; // getMonth() returns a zero-based index
-              
-              // Format the date as "14, Sept."
+              const month = months[date.getMonth()];
               const formattedDate = `${day}, ${month}`;
-              return(
+
+              return (
                 <div key={blog.id} className="col-md-6">
-                <div className="blog-post-item-two">
-                  <div className="blog-post-thumb-two">
-                    <Link to={`/blog-details/${blog.id}`}>
-                      <img src={blog.featured_images[0].blog_image} alt="" />
-                    </Link>
-
-                    <Link to={`/blog-details/${blog.id}`} className="tag tag-two">
-                      {blog.categories[0].name}
-                    </Link>
-                  </div>
-
-                  <div className="blog-post-content-two">
-                    <h2 className="title">
-                      <Link to={`/blog-details/${blog.id}`}>{blog.title}</Link>
-                    </h2>
-                    <p>{blog.content}</p>
-
-                    <div className="blog-meta">
-                      <ul className="list-wrap">
-                        <li>
-                          <Link to={`/blog-details/${blog.id}`}>
-                            {/* <img src={blog.authorSrc} alt="" /> */}
-                            {blog.created_by}
-                          </Link>
-                        </li>
-                        <li>
-                          <i className="far fa-calendar"></i>
-                          {formattedDate}
-                        </li>
-                      </ul>
+                  <div className="blog-post-item-two">
+                    <div className="blog-post-thumb-two">
+                      <Link to={`/blog-details/${blog.id}`}>
+                        <img src={blog.featured_images[0].blog_image} alt="" />
+                      </Link>
+                      <Link to={`/blog-details/${blog.id}`} className="tag tag-two">
+                        {blog.categories[0].name}
+                      </Link>
+                    </div>
+                    <div className="blog-post-content-two">
+                      <h2 className="title">
+                        <Link to={`/blog-details/${blog.id}`}>{blog.title}</Link>
+                      </h2>
+                      <p className="truncate-text">{blog.content}</p>
+                      <div className="blog-meta">
+                        <ul className="list-wrap">
+                          <li>
+                            <Link to={`/blog-details/${blog.id}`}>
+                              {blog.created_by}
+                            </Link>
+                          </li>
+                          <li>
+                            <i className="far fa-calendar"></i>
+                            {formattedDate}
+                          </li>
+                        </ul>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              )
+              );
             })}
           </div>
 
@@ -90,33 +90,20 @@ const BlogPage = () => {
           <div className="pagination-wrap mt-30">
             <nav aria-label="Page navigation example">
               <ul className="pagination list-wrap">
-                <li className="page-item">
-                  <a className="page-link" href="#">
+                <li className="page-item" onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
+                  <a className="page-link" href="#" aria-disabled={currentPage === 1}>
                     <i className="fas fa-angle-double-left"></i>
                   </a>
                 </li>
-                <li className="page-item active">
-                  <a className="page-link" href="#">
-                    1
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="#">
-                    2
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="#">
-                    3
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="#">
-                    4
-                  </a>
-                </li>
-                <li className="page-item next-page">
-                  <a className="page-link" href="#">
+                {[...Array(totalPages)].map((_, index) => (
+                  <li key={index} className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}>
+                    <a className="page-link" href="#" onClick={() => handlePageChange(index + 1)}>
+                      {index + 1}
+                    </a>
+                  </li>
+                ))}
+                <li className="page-item" onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages}>
+                  <a className="page-link" href="#" aria-disabled={currentPage === totalPages}>
                     <i className="fas fa-angle-double-right"></i>
                   </a>
                 </li>
